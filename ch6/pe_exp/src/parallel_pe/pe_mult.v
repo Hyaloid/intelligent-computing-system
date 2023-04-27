8c63ba52bc69a253728ece23a502bd4d9ba6f2d2
hyaloid <hyaloids@163.com>
2023-04-27 23:06:35 +0800

    pass parallel_pe

--- ch6/pe_exp/src/parallel_pe/pe_mult.v
+++ ch6/pe_exp/src/parallel_pe/pe_mult.v
@@ -14,10 +14,10 @@ wire signed [31:0] int16_mult_result[31:0];
 generate
   for(i=0; i<32; i=i+1)
   begin:int16_mult                 /* TODO */
-    _________________________________________
-    _________________________________________
-    _________________________________________
-    _________________________________________
+    assign int16_neuron[i][15:0] = mult_neuron[15+16*i:0+16*i];
+    assign int16_weight[i][15:0] = mult_weight[15+16*i:0+16*i];
+    assign int16_mult_result[i] = int16_neuron[i] * int16_weight[i];
+    assign mult_result[31+32*i:0+32*i] = int16_mult_result[i];
   end
 endgenerate
 
