43844d4e7b45f3372dfd708f95e70606a951c59d
hyaloid <hyaloids@163.com>
2023-04-28 11:09:58 +0800

    pass matrix_pe

--- ch6/pe_exp/src/matrix_pe/matrix_pe.v
+++ ch6/pe_exp/src/matrix_pe/matrix_pe.v
@@ -14,68 +14,71 @@ module matrix_pe(
   output                vld_o
 );
 
+
+wire [1:0] pe_ctl;
 reg inst_vld;
+wire pe_vld_i = inst_vld && nram_mpe_neuron_valid && wram_mpe_weight_valid;
+
 reg [7:0] inst;  /*inst存放输入控制信号ib_ctl_uop的值*/
 always@(posedge clk or negedge rst_n) begin
   /* TODO: inst_vld & inst */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  if(!rst_n) begin
+    inst <= 1'b0;
+    inst_vld <= 1'b0;
+  end else if(ib_ctl_uop_valid && nram_mpe_neuron_valid && wram_mpe_weight_valid) begin
+    inst <= ib_ctl_uop;
+    inst_vld <= 1'b1;
+  end else if(pe_ctl[1] && pe_vld_i) begin
+    inst <= 1'b0;
+    inst_vld <= 1'b0;
+  end
 end
 
-wire pe_vld_i = inst_vld && nram_mpe_neuron_valid && wram_mpe_weight_valid;
 reg [7:0] iter;
 
 always@(posedge clk or negedge rst_n) begin
   /* TODO: iter */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  if(!rst_n) begin
+    iter <= 8'b0;
+  end else if(pe_ctl[1] && pe_vld_i ) begin
+    iter <= 8'b0;
+  end else if(pe_vld_i && nram_mpe_neuron_valid && wram_mpe_weight_valid) begin
+    iter <= iter + 1'b1;
   end
 end
 
 always@(posedge clk or negedge rst_n) begin
   /* TODO: ib_ctl_uop_ready */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  if(!rst_n) begin
+    ib_ctl_uop_ready <= 1'b0;
+  end else if(pe_ctl[1]) begin
+    ib_ctl_uop_ready <= 1'b1;
+  end else if(ib_ctl_uop_ready) begin
+    ib_ctl_uop_ready <= 1'b0;
+  end
 end
 
 wire [511:0] pe_neuron = nram_mpe_neuron;
 wire [511:0] pe_weight = wram_mpe_weight;
-wire [1:0] pe_ctl;
-assign pe_ctl[0] = _________________________________;  /* TODO */
-assign pe_ctl[1] = _________________________________;  /* TODO */
+assign pe_ctl[0] = (iter[7:0] == 8'h0) && nram_mpe_neuron_valid && wram_mpe_weight_valid;  /* TODO */
+assign pe_ctl[1] = (iter[7:0] == (inst[7:0] - 1'b1)) && nram_mpe_neuron_valid && wram_mpe_weight_valid;  /* TODO */
 
 wire [31:0] pe_result;
 wire pe_vld_o;
 parallel_pe u_parallel_pe (
 /* TODO */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  .clk                  (clk      ),
+  .rst_n                (rst_n    ),
+  .neuron               (pe_neuron),
+  .weight               (pe_weight),
+  .ctl                  (pe_ctl   ),
+  .vld_i                (pe_vld_i ),
+  .result               (pe_result),
+  .vld_o                (pe_vld_o )
 );
 
-assign nram_mpe_neuron_ready = _________________________________;  /* TODO */
-assign wram_mpe_weight_ready = _________________________________;  /* TODO */
+assign nram_mpe_neuron_ready = pe_vld_i;  /* TODO */
+assign wram_mpe_weight_ready = pe_vld_i;  /* TODO */
 
 assign result = pe_result;
 assign vld_o = pe_vld_o;
