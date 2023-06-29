8b4be1753d2e337b086146e582a71dfb8469ee56
hyaloid <hyaloids@163.com>
2023-06-29 17:10:28 +0800

    pass ch5-1

--- ch5/code_chap_5_1_student/src/bangc/PluginPowerDifferenceOp/plugin_power_difference_kernel.h
+++ ch5/code_chap_5_1_student/src/bangc/PluginPowerDifferenceOp/plugin_power_difference_kernel.h
@@ -30,7 +30,7 @@ extern "C" {
 typedef uint16_t half;
 
 // TODO：BCL接口定义
-void PowerDifferenceKernel(---);
+void PowerDifferenceKernel(half *input1, half *input2, int32_t pow, half *output, int32_t len);
 
 #ifdef __cplusplus
 }
